2f380df1d68d8415e4e48f5c234202a66d261fa5
kike <yeryanyin@gmail.com>
2026-05-05 08:52:24 -0400

    Update code.py

--- code.py
+++ code.py
@@ -4,26 +4,27 @@ import random
 class Hero:
     def __init__(self, name, health, balance, inventory, stats):
         self.name = name
-        self.healh = health
+        self.health = health
         self.balance = balance
+        self.inventory = inventory
+        self.stats = stats
 
-        
 window = tk.Tk()
 window.title("OOP Adventures") 
-window.geometry("400x250") 
+window.geometry("1200x800") 
 window.resizable(False, False)
 
 prompt = tk.Label(window, text="Type your message below:",
-font=("Calibri", 14))
-prompt.pack(pady=10)
+font=("Calibri", 28))
+prompt.pack(pady=20)
 
-entry = tk.Entry(window, font=("Calibri", 14), width=30)
-entry.pack(pady=5)
+entry = tk.Entry(window, font=("Calibri", 28), width=60)
+entry.pack(pady=10)
 
 
-result_label = tk.Label(window, text="", font=("Calibri", 14, "bold"),
+result_label = tk.Label(window, text="", font=("Calibri", 28, "bold"),
 fg="blue")
-result_label.pack(pady=15)
+result_label.pack(pady=30)
 
 
 window.mainloop()
