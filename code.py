9bfecf976e66782ee8be5c968a9d1e72924d81f1
KakeguriMashoBayou <boyul@nycstudents.net>
2026-05-21 07:08:34 -0400

    Update code.py

--- code.py
+++ code.py
@@ -24,46 +24,48 @@ class Market:
         self.inflation = inflation
 
     def check(self, market_items):
-        for e in market_items:
-            print(e["name"])
-    
-    def buy(self, balance, inflation, inventory):
-        item = input("What would you like to buy in the Market?""\n")
-        while (item != "exit") or (item != "end"):
-            if item == "check":
-               Market.check(item_data)
-            elif item == "inventory":   
-                print(inventory)
-            else:
-                for i in item_data:
-                    if item == i["name"]:
-                        Veliky_Tarnovo = True
-                        print("Item Price:",int(i["price"]),"\n","Your balance:", balance, "\n")
-                        print("Inflation", inflation, "\n")
-                        try:
-                            balance - int(i["price"]*inflation)
-                        except int(inflation*i["price"]) > int(balance):
-                            print("You cannot buy this item""\n")
+        for item in market_items:
+            print(f"{item['name']} - Price: {item['price']}")
+            
+            def buy(self,balance, inventory):
+                items = {item["name"]: item for item in item_data}
+                while True:
+                    item = input("what would you like to buy from the market\n").lower()
+                    if item in ["exit", "end"]:
+                        break
+                    elif item == "check":
+                        self.check(item_data)
+                    elif item == "inventory":
+                        print(inventory)
+                    elif item in items:
+                        selected = items[item]
+                        final_price = int(selected["price"] * self.inflation)
+                        
+                        print(f"item price: {final_price}")
+                        print(f"your balance: {balance}")
+                        print(f"inflation: {self.inflation}\n")
+                        if final_price > balance:
+                            print("you dont have enough money.\n")
+                            continue
+                        if len(inventory) >= 5:
+                            print("Max inventory.\n")
+                            continue
+                        balance -= final_price
+                        if item == "health potion":
+                            Hero.health += 20
+                        elif item == "speed potion":
+                            Hero.speed += 20
+                        elif item == "strength potion":
+                            Hero.strength += 20
                         else:
-                            inventory_amount = len(inventory)
-                            if inventory_amount < 5:
-                                balance -= int(i["price"]*inflation)
-                                if item == "health potion":
-                                    Hero.health += 20
-                                elif item == "speed potion":
-                                    Hero.speed +=20
-                                elif item == "strength potion":
-                                    Hero.strength +=20
-                                else:
-                                    inventory.append(item)
-                                    print(f"{i["name"]} was purchased!""\n")
-                                    return balance
-                            else:
-                                print("You have too much items in your inventory.")
-                            return inventory
-                if Veliky_Tarnovo == False:
-                    print("No item found.")
-            item = input("What else would you like to do?")
+                            inventory.append(item)
+                        print(f"{selected['name']} was bouht\n")
+                    else:
+                        print("no item is called that.\n")
+                return balance, inventory
+                            
+                        
+
 
 class Enemy:
     def __init__(self):
