c8cacfd6cdc6d75c112f85769112cc42ab681fdc
kike <yeryanyin@gmail.com>
2026-05-26 09:26:05 -0400

    Update code.py

--- code.py
+++ code.py
@@ -176,13 +176,21 @@ inventorystatus.pack(pady=10)
 inventorystatus.place(x=800, y=600)
 
 while Elias.health > 0:
-    pass
     if Elias.health < Elias.maxhealth:
         Elias.health += (Elias.maxhealth*0.05)
     elif Elias.health > Elias.maxhealth:
             Elias.health = Elias.maxhealth
     Abstreich.random_event_modifier
-    Breunat.buy()
+        print("what do you want to do?""\n""[1] check current health""\n""[2] check current speed""\n""[3] check current strength""\n""[4] open the market")
+    menu_input = int(input("What do you want to do?"))
+    if menu_input == 1:
+        print(Hero.Health)
+    elif menu_input == 2:
+        print(Hero.Speed)
+    elif menu_input == 3:
+        print(Hero.Strength)
+    elif menu_input == 4:
+        print("Okay, browsing the market")
     pass
 print(Elias.name, "has died! Your final score is", Elias.stats)
 
